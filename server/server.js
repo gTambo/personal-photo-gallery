74eb630cfb9b32d1f7a8e10599018ea65cd4170e
Greg Tambo <gregtambornino@gmail.com>
2021-10-12 01:25:46 -0500

    initial s3bucket image uploading with url

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+require('dotenv').config();
 const gallery = require('./routes/gallery.router.js');
 const PORT = process.env.PORT || 5000;
+
+const UploaderS3router = require('react-dropzone-s3-uploader/s3router');
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for axios requests
@@ -10,6 +13,14 @@ app.use(express.static('build'));
 
 /** ---------- EXPRESS ROUTES ---------- **/
 app.use('/gallery', gallery);
+
+app.use('/s3', UploaderS3router({
+    bucket: 'climbtags1',                           // required change to correct bucket name
+    region: 'us-east-2',                            // optional
+    headers: {'Access-Control-Allow-Origin': '*'},  // optional
+    ACL: 'public-read',                                 // this is the default - set to `public-read` to let anyone view uploads
+  }));
+  
 
 /** ---------- START SERVER ---------- **/
 app.listen(PORT,  () => {
